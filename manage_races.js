95c8e7a3b8f877e0458b40da9b071eac301ece1a
filemon <jiri.fabian@gmail.com>
2021-03-29 10:56:35 +0200

    Fixing lap and screen width for races

--- manage_races.js
+++ manage_races.js
@@ -1,4 +1,5 @@
 const nextButtonSelector = 'div.moreButton[id="snippet-moreButtonPlanned-component"] > a';
+const filterButtonSelector = 'input.btn.btn-success';
 
 function getChallenges() {
     console.log('Getting challenges');
@@ -9,17 +10,6 @@ function getChallenges() {
     });
 }
 
-function createButton() {
-    let button = document.createElement('button');
-    button.type = "button";
-    button.className = 'btn btn-info';
-    button.onclick = async function () {
-        await filterRaces();
-    };
-    button.style.marginTop = '10px';
-    button.innerHTML = 'show official only';
-    return button;
-}
 
 function getRaceDetails() {
     console.log('Getting race details');
@@ -39,27 +29,17 @@ function getCareerDetails() {
     });
 }
 
-async function filterRaces() {
-    for(let i=0;i<5;i++){
-        try {
-            await $(nextButtonSelector)[0].click();
-            // Default timeout first time.
-            await new Promise(function(resolve) {setTimeout(resolve, 2000)});
-            // 2 sec timeout after the first.
-        } catch (err) {
-            // Ignore the timeout error.
-            console.log(`Error: ${err}`);
-        }
-    }
-    $('div.planned div.avatar22 a').not('a[href="/ROUVY"]').closest('tr').remove();
-};
-
-function filterRegistrations(race_details) {
+async function filterRegistrations() {
+    let race_details = await getRaceDetails();
     $('div.tableVT.invitations a').each(function() {
-            let race_link =$(this).attr('href');
+            let race_link =remove_special_parameters($(this).attr('href'));
             let race = race_details['races'][race_link];
+            let invitation = $(this).closest('tr');
             if(!race) { //remove old invitations
-                $(this).closest('tr').remove();
+                invitation.remove();
+            } else {
+                let date = new Date(race.date + 'Z'); //handle locale time
+                appendField(invitation[0], invitation[0].children[1], 'td', `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`);
             }
     });
 }
@@ -69,11 +49,15 @@ function appendField(header, sibling, tag, text) {
     field.innerHTML = text;
     header.insertBefore(field, sibling);
 }
+function removeField(element) {
+    element.remove();
+}
 
 function updateTableHeader() {
     if($('div.tabcont.oncont.box.planned thead th:contains("Ascended")').length == 0) {
         const header_row = $('div.tabcont.oncont.box.planned thead tr')[0];
         if(header_row) { //we can be on the race detail
+            removeField(header_row.children[0]);//remove status
             appendField(header_row, header_row.children[4], 'th', "Estimated time (~2 W/kg)");
             appendField(header_row, header_row.children[4], 'th', "Challenge");
             appendField(header_row, header_row.children[4], 'th', "Carreer");
@@ -89,6 +73,7 @@ function updateTableHeader() {
 
 function updateRaceDetail(row,details,carreer, challenges) {
     if($(row).children('td:contains("%")').length == 0) {
+        removeField(row.children[0]);//remove status
         appendField(row, row.children[4], 'td', details.estimated_time);
         appendField(row, row.children[4], 'td', challenges);
         appendField(row, row.children[4], 'td', carreer);
@@ -105,13 +90,16 @@ function remove_special_parameters(link) {
     return link.split('?')[0];
 }
 
+function updateContentWidth() {
+    $('div.events')[0].style.width="1400px";
+}
+
 async function enrichDetails() {
+    updateContentWidth();
     updateTableHeader();
     let race_details = await getRaceDetails();
     let career = await getCareerDetails();
     let challenges = await getChallenges();
-
-    filterRegistrations(race_details);
 
     $('div.planned div.avatar22 a').closest('tr').each(function() {
        let race_link = $($(this).find('a.btn')).attr('href');
@@ -161,16 +149,19 @@ function challengeRaces(challenges,race_link) {
     return ret.join();
 }
 
-function adjustNextButton() {
+function adjustButtons() {
     let button = $(nextButtonSelector)[0];
+    let button2 = $(filterButtonSelector)[0];
     if(button) {
+        console.log('Adjusting buttons');
         button.onclick = async function () {
             await new Promise(function (resolve) {
                 setTimeout(resolve, 2000)
             });
-            adjustNextButton(); //register event handler on fresh button;
+            adjustButtons(); //register event handler on fresh button;
             await enrichDetails();
         };
+        button2.onclick = button.onclick;
     }
 }
 
@@ -180,6 +171,7 @@ function translateString(input) {
 
 
 (async () => {
+    await filterRegistrations();
     await enrichDetails(); //enrich details on already loaded page
-    adjustNextButton(); //enrich them after each next button click
+    adjustButtons(); //enrich them after each next button click
 })();
